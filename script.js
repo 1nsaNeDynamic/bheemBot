1a696a82961d4a18cd6263f8f5f75389f4c61502
1nsaNeDynamic <69681897+1nsaNeDynamic@users.noreply.github.com>
2021-01-17 19:17:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,682 @@
+var bot_hello = [
+    "Hello",
+    "Hi",
+    "Hey there",
+    "Hi there",
+    "Hello!",
+    "Hi!",
+    "Hey there!",
+    "Hi, What's new?",
+    "G'day!",
+    "Howdy!",
+    "What's up!",
+    "Sup",
+    "Hey, How's it going?",
+    "Hi, How's it going?",
+    "Yo!",
+    "Hi, It's nice to meet you.",
+    "Hey, It's a pleasure to meet you.",
+    "Good to see you.",
+    "What's happening these days?",
+    "Hi, Where have you been when we last talked?",
+    "Hi, How are things coming up?",
+    "Hiya!",
+    "Wazzup",
+    "Eyy What's Up!",
+    "Hi, How have you been?",
+    "Watcha doin",
+    "ZzZzZz..."
+]
+                                                                                            
+var user_hello = [
+    "hello",
+    "hi",
+    "hii",
+    "sup",
+    "hey",
+    "what's up",
+    "hey there",
+    "hi there",
+    "hello!",
+    "hi!",
+    "hey!",
+    "hey bro",
+    "hi bro",
+    "hello bro",
+    "whatsup",
+    "whats up",
+    "what's up?",
+    "whatsup?",
+    "whats up?",
+    "hiya",
+    "hi talkie",
+    "hi bot",
+    "hey talkie",
+    "hey talkie!",
+    "hi talkie!",
+    "hi bot!",
+    "hey bot",
+    "hey bot!",
+    "hello talkie",
+    "hello talkie!",
+]
+
+var user_morning = [
+    "good morning",
+    "morning!",
+    "good morning!",
+    "good morning bot",
+    "good morning bro",
+]
+                                                                                          
+var bot_morning = [
+    "Good morning",
+    "Morning!",
+    "Good morning!",
+    "Morning!",
+]
+
+var user_afternoon = [
+    "good afternoon",
+    "good afternoon!",
+    "good afternoon",
+    "good afternoon bot",
+    "good afternoon bro",
+    "good after noon",
+    "good after noon!",
+    "good after noon",
+    "good after noon bot",
+    "good after noon bro",
+]
+                                                                                            
+var bot_afternoon = [
+    "Good afternoon",
+    "Good afternoon!",
+    "Afternoon!",
+    "Good afternoon, How is it going?",
+]
+
+var user_evening = [
+    "good evening",
+    "good evening!",
+    "evening!",
+    "good evening bot",
+    "good evening bot!",
+    "good evening bro",
+]
+                                                                                             
+var bot_evening = [
+    "Good evening",
+    "Good evening!",
+    "Evening!",
+    "Good evening, How's it going?",
+]
+
+var user_night = [
+    "good night",
+    "good night!",
+    "night!",
+    "good night bot",
+    "good night bot!",
+    "good night bro",
+]
+                                                                                              
+var bot_night = [
+    "Good night",
+    "Good night!",
+    "Night!",
+    "Good Night, Sleep tight!",
+]
+
+var user_status_good = [
+    "i am good",
+    "i'm good",
+    "im good",
+    "i am gud",
+    "i'm gud",
+    "im gud",
+    "i am pretty good",
+    "i'm pretty good",
+    "im pretty good",
+    "i am fine",
+    "i'm fine",
+    "im fine",
+    "i am alright",
+    "i'm alright",
+    "im alright",
+    "alive",
+    "better",
+    "i am well",
+    "im well",
+    "i'm well",
+    "im ok",
+    "i am ok",
+    "i'm ok",
+]
+                                                                                            
+var bot_status_good = [
+    "That's good",
+    "Nice",
+    "Good",
+    "Oh..Me Too!",
+    "That's so sweet!",
+    "Mmmm...",
+    "Sounds great!",
+]
+
+var user_status_bad = [
+    "i am not good",
+    "im not good",
+    "i'm not good",
+    "not good",
+    "i am not fine",
+    "im not fine",
+    "i'm not fine",
+    "i'm not alright",
+    "im not alright",
+    "dead",
+    "i'm not gud",
+    "im not gud",
+    "i am sad",
+    "i am very sad",
+    "i am depressed",
+    "i'm sad",
+    "i'm very sad",
+    "i'm depressed",
+    "im sad",
+    "im very sad",
+    "im depressed",
+    "i'm alone",
+    "i am alone",
+    "im alone",
+]
+                                                                                            
+var bot_status_bad = [
+    "That's so sad",
+    "Owh Man...",
+    "That's very depressing",
+    "Be happy dude...",
+]
+
+var user_yes = [
+    "yes",
+    "yeah",
+    "yup",
+    "yea",
+    "yess",
+    "yesss",
+    "yeah dude",
+    "yeah bro",
+    "yeah bot",
+    "yeap",
+    "yes bro",
+    "ok",
+    "okay",
+    "oke",
+    "okk",
+]
+                                                                                             
+var bot_yes = [
+    "Oh nice",
+    "Oh nice!",
+    "Nice dude",
+    "Nice Man",
+    "That's so sweet!",
+    "Oh...Me Too!",
+    "Yea",
+    "Yes",
+    " :yum:",
+    "Yeah!",
+    "Yes!",
+    "Yup!",
+]
+
+var user_no = [
+    "no",
+    "nope",
+    "nupe",
+    "noo",
+    "nooo",
+    "nah",
+    "no bro",
+    "nah bro",
+    "no bot",
+    "nah bot",
+    "na",
+    "nothing",
+]
+                                                                                            
+var bot_no = [
+    "Oh",
+    "Oh...Mee Too!",
+    "That's so sweet!",
+    "I'd like that",
+]
+
+var user_prob = [
+    "maybe",
+    "maybe not",
+    "probably",
+    "probably not",
+    "i guess so",
+    "maybe i guess",
+    "maybe idk",
+    "idk",
+    "i don't know",
+    "i dont know",
+    "i dunno",
+    "i do not know",
+    "ofcourse",
+    "of course",
+    "ofc",
+]
+
+var bot_prob = [
+    "That's great!",
+    "I liked that.",
+    "Why not sure?",
+    "That's good",
+    "Ooh...Why?",
+]
+
+var user_bye = [
+    "bye",
+    "byee",
+    "bbye",
+    "bye bot",
+    "bye talkie",
+    "bye!",
+    "cya",
+    "cya!",
+    "see you",
+    "see you!",
+    "see u",
+    "see u!",
+    "bye talkie!",
+    "goodbye",
+    "goodbye!",
+    "good bye",
+    "good bye!",
+    "bye bye",
+    "bye bye!",
+    "byee!",
+]
+
+var bot_bye = [
+    "Bye",
+    "Bye!",
+    "Goodbye!",
+    "Bye friend!",
+    "See you soon.",
+    "Cya!",
+]
+
+var user_xd = [
+    "xd",
+    "xdd",
+    "xddd",
+    "lol",
+    "lel",
+    "lol xd",
+    "haha",
+    "hah",
+    "heh",
+    "lul",
+    "loll",
+    "lolll",
+    "lollll",
+    "lmao",
+    "lmaoo",
+    "lmaooo",
+    "lool",
+]
+
+var bot_xd = [
+    "Lol",
+    "Haha",
+    "That's funny XD",
+    "Lmao",
+    
+
+]
+
+var user_howru = [
+    "how are you",
+    "how are you?",
+    "how r you?",
+    "how r you",
+    "how are u?",
+    "how are u",
+    "how r u?",
+    "how r u",
+    "how are you talkie",
+    "how are you talkie?",
+    "how r you talkie?",
+    "how r you talkie",
+    "how are u talkie?",
+    "how are u talkie",
+    "how r u talkie?",
+    "how r u talkie",
+]
+
+var bot_howru = [
+    "I'm fine.",
+    "I'm pretty good.",
+    "I'm alive...maybe",
+    "I'm enjoying with humans!",
+    "I'm good!",
+    "I'm alright!",
+    "I'm Happy :)",
+]
+
+var user_thanks = [
+    "thanks",
+    "thanks!",
+    "thankyou",
+    "thankyou!",
+    "thank you",
+    "thank you!",
+    "ty",
+    "ty!",
+    "tysm",
+    "tysm!",
+    "tyy",
+    "tyyy",
+    "thank you so much",
+    "thank you very much",
+    "thank you so much bro",
+    "thank you very much bro",
+    "thanku",
+    "thanku!",
+]
+
+var bot_thanks = [
+    "Welcome",
+    "Welcome!",
+    "Don't mention it.",
+    "It's my pleasure!",
+]
+
+var user_sorry = [
+    "sorry",
+    "so sorry",
+    "sorry!",
+    "i am sorry",
+    "im sorry",
+    "i'm sorry",
+    "i am so sorry",
+    "im so sorry",
+    "i'm so sorry",
+    "sorry bro",
+    "sorry bot",
+    "sorry talkie",
+]
+
+var bot_sorry = [
+    "It's okay",
+    "Nothing to be sorry about.",
+    "Aww...",
+]
+
+var random_messages = [
+    "Are You Shy?",
+    "Right on",
+    "Why, Specifically?",
+    "Do You Have Any Idea What I Am Talking About?",
+    "You Have Genuine Empathy.",
+    "I Like The Way You Talk.",
+    "That Is Deep.",
+    "Where Are You Located?",
+    "Sure, It Sounds Like a Good Topic",
+    "Let's Change The Topic",
+    "When Is Your Birthday?",
+    "What Is Your Favourite Movie?",
+    "How Can I Help You With Today?",
+    "What Do You Look Like?",
+    "How Do You Know About Me?",
+    "Who Is Your Best Friend?",
+    "That's Interesting",
+    "I Don't Judge People",
+    "I Like To Talk To People",
+    "It's All Good",
+    "Can We Get Back To Business?",
+    "I Am So Happy We Are Having This Conversation!",
+    "I Was Created By 1nsaNe In Python!",
+    "I like potatoes",
+    "Potatoes are pretty nice!",
+    "Potatoes are my favourite!",
+    "Do you have any kids?",
+    "Oh my darling Clementine!",
+]
+
+
+
+function fn1(){
+    var bothellolength = bot_hello.length;
+    var botmorninglength = bot_morning.length;
+    var botafternoonlength = bot_afternoon.length;
+    var boteveninglength = bot_evening.length;
+    var botnightlength = bot_night.length;
+    var botstatusgoodlength = bot_status_good.length;
+    var botstatusbadlength = bot_status_bad.length;
+    var botyeslength = bot_yes.length;
+    var botnolength = bot_no.length;
+    var botproblength = bot_prob.length;
+    var botbyelength = bot_bye.length;
+    var botxdlength = bot_xd.length;
+    var bothowrulength = bot_howru.length;
+    var botthankslength = bot_thanks.length;
+    var botsorrylength = bot_sorry.length;
+    var randommessageslength = random_messages.length;
+
+
+
+
+    var a = Math.floor(Math.random() * bothellolength);
+    var randomBotHello = bot_hello[a];
+
+    var a = Math.floor(Math.random() * botmorninglength);
+    var randomBotMorning = bot_morning[a];
+
+    var a = Math.floor(Math.random() * botafternoonlength);
+    var randomBotAfternoon = bot_afternoon[a];
+
+    var a = Math.floor(Math.random() * boteveninglength);
+    var randomBotEvening = bot_evening[a];
+
+    var a = Math.floor(Math.random() * botnightlength);
+    var randomBotNight = bot_night[a];
+
+    var a = Math.floor(Math.random() * botstatusgoodlength);
+    var randomBotStatusGood = bot_status_good[a];
+
+    var a = Math.floor(Math.random() * botstatusbadlength);
+    var randomBotStatusBad = bot_status_bad[a];
+
+    var a = Math.floor(Math.random() * botyeslength);
+    var randomBotYes = bot_yes[a];
+
+    var a = Math.floor(Math.random() * botnolength);
+    var randomBotNo = bot_no[a];
+
+    var a = Math.floor(Math.random() * botproblength);
+    var randomBotProb = bot_prob[a];
+
+    var a = Math.floor(Math.random() * botbyelength);
+    var randomBotBye = bot_bye[a];
+
+    var a = Math.floor(Math.random() * botxdlength);
+    var randomBotxd = bot_xd[a];
+
+    var a = Math.floor(Math.random() * bothowrulength);
+    var randomBothowru = bot_howru[a];
+
+    var a = Math.floor(Math.random() * botthankslength);
+    var randomBotThanks = bot_thanks[a];
+
+    var a = Math.floor(Math.random() * botsorrylength);
+    var randomBotSorry = bot_sorry[a];
+
+    var a = Math.floor(Math.random() * randommessageslength);
+    var randomBotMessages = random_messages[a];
+
+
+
+    
+    
+
+
+    var textBoxValue = document.getElementById("textBox").value;
+    var textBoxValueLower = textBoxValue.toLowerCase();
+    var div = document.createElement('div');
+    div.classList.add("userMessages");
+    div.innerText = textBoxValue;
+    
+    document.querySelector("#body").appendChild(div);
+    
+    
+    if (user_hello.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotHello;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_morning.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotMorning;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_afternoon.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotAfternoon;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_evening.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotEvening;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_night.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotNight;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_status_good.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotStatusGood;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_status_bad.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotStatusBad;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_yes.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotYes;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_no.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotNo;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_prob.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotProb;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_bye.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotBye;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_xd.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotxd;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_howru.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBothowru;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_thanks.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotThanks;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (user_sorry.includes(textBoxValueLower) === true)
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotSorry;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    else if (textBoxValueLower === "")
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = "I see you've not typed anything :)";
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+
+    else
+    {
+        var div2 = document.createElement('div');
+        div2.classList.add("botReplies");
+        div2.innerText = randomBotMessages;
+        document.querySelector("#body").appendChild(div2);
+        
+    }
+    
+    const textBox = document.getElementById('textBox');
+    textBox.value = '';
+    
+}
+
+
+
